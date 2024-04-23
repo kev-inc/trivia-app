f713bf55fdce7882780fe16c64fe5220e70ce7c7
kev-inc <kevinchanxy@gmail.com>
2024-04-23 16:15:01 +0000

    tests

---
+++ test.js
@@ -0,0 +1,66 @@
+const { io } = require("socket.io-client");
+
+const URL = process.env.URL || "http://localhost:3001";
+const MAX_CLIENTS = 100;
+const POLLING_PERCENTAGE = 0.05;
+const CLIENT_CREATION_INTERVAL_IN_MS = 10;
+const EMIT_INTERVAL_IN_MS = 1000;
+
+let clientCount = 0;
+let lastReport = new Date().getTime();
+let packetsSinceLastReport = 0;
+
+const randomUser = () => {
+    return "user" + Math.floor(Math.random() * (100000))
+}
+
+const createClient = () => {
+  // for demonstration purposes, some clients stay stuck in HTTP long-polling
+  const transports =
+    Math.random() < POLLING_PERCENTAGE ? ["polling"] : ["polling", "websocket"];
+
+  const socket = io(URL, {
+    transports,
+  });
+
+  socket.emit('joinGame', {username: randomUser()})
+
+//   setInterval(() => {
+//     // socket.emit("client to server event");
+//     socket.emit('joinGame', { username: randomUser()})
+//   }, EMIT_INTERVAL_IN_MS);
+
+//   socket.on("server to client event", () => {
+//     packetsSinceLastReport++;
+//   });
+  socket.on('updatePlayers', () => {
+    packetsSinceLastReport++;
+  })
+
+  socket.on("disconnect", (reason) => {
+    console.log(`disconnect due to ${reason}`);
+  });
+
+  if (++clientCount < MAX_CLIENTS) {
+    setTimeout(createClient, CLIENT_CREATION_INTERVAL_IN_MS);
+  }
+};
+
+createClient();
+
+const printReport = () => {
+  const now = new Date().getTime();
+  const durationSinceLastReport = (now - lastReport) / 1000;
+  const packetsPerSeconds = (
+    packetsSinceLastReport / durationSinceLastReport
+  ).toFixed(2);
+
+  console.log(
+    `client count: ${clientCount} ; average packets received per second: ${packetsPerSeconds}`
+  );
+
+  packetsSinceLastReport = 0;
+  lastReport = now;
+};
+
+setInterval(printReport, 5000);
