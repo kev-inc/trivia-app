e8a1abb6e91c23375222419f8071256de1a0a60d
kev-inc <kevinchanxy@gmail.com>
2024-05-09 02:27:57 +0000

    update qr

--- frontend/src/components/screen/WaitingScreen.jsx
+++ frontend/src/components/screen/WaitingScreen.jsx
@@ -3,7 +3,7 @@ import qr from '../../images/qr.png'
 const WaitingScreen = ({players}) => (
     <div key={0}>
         <div className='flex justify-center'>
-            <span className='text-2xl p-4 font-playfair font-semibold tracking-wider bg-white text-black rounded mb-4'>Scan me to join, or go to https://knj-trivia.pages.dev/</span>
+            <span className='text-2xl p-4 font-playfair font-semibold tracking-wider bg-white text-black rounded mb-4'>Scan to join, or go to https://wedding.kevincxy.com/</span>
         </div>
         <div className='flex justify-center'>
             <img className='h-64' src={qr} alt="qr"/>
