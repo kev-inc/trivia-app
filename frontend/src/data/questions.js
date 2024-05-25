fa1e29c9b20317ee795ae1cfe1c9284a8d8414aa
kev-inc <kevinchanxy@gmail.com>
2024-05-25 03:06:41 +0000

    update questions

--- frontend/src/data/questions.js
+++ frontend/src/data/questions.js
@@ -1,22 +1,44 @@
 export const questions = [
-    {
-        question: 'Where did Kevin & Jewel first meet?',
-        answers: ['At work', 'At university', 'At a cafe', 'At a bus stop'],
-        answer: 1
-    },
-    {
-        question: 'When did Kevin propose to Jewel?',
-        answers: ['11 Nov 2023', '25 Dec 2023', '1 Jan 2024', '2 Feb 2024'],
-        answer: 1
-    },
-    {
-        question: 'What is Kevin & Jewel love language?',
-        answers: ['Quality time', 'Words of affirmation', 'Acts of service', 'All of the above'],
-        answer: 0
-    },
-    {
-        question: 'Where did Kevin & Jewel first go overseas together?',
-        answers: ['Japan', 'Korea', 'London', 'Paris'],
-        answer: 0
-    },
+
+{
+	question: 'Where did Kevin & Jewel first meet?',
+	answers: ['At work', 'At university', 'At a cafe', 'At the gym'],
+	answer: 1
+},
+{
+	question: 'Where did Kevin & Jewel go on their first date?',
+	answers: ['Ice Skating', 'Artbox', 'Pottery Workshop' ,'Disney Tsum Tsum Festival'],
+	answer: 3
+},
+{
+	question: 'When did Kevin propose to Jewel?',
+	answers: ['28 Jul 2023', '11 Nov 2023', '25 Dec 2023', '1 Jan 2024'],
+	answer: 2
+},
+{
+	question: "What is Kevin & Jewel's ideal date?",
+	answers: ['Chilling at home', 'Cafe hopping', 'Going on walks', 'Playing board games'],
+	answer: 0
+},
+{
+	question: 'What is one thing that Jewel likes but Kevin dislikes?',
+	answers: ['Yoga', 'Matcha', 'Instagram', 'Waking up early'],
+	answer: 1
+},
+{
+	question: 'What is one thing that Kevin likes but Jewel dislikes?',
+	answers: ['Running', 'Spicy food', 'Microsoft Excel', 'Going to sleep early'],
+	answer: 1
+},
+{
+	question: "Where is Kevin & Jewel's dream honeymoon destination?",
+	answers: ['London', 'Paris', 'Switzerland', 'New Zealand'],
+	answer: 3
+},
+{
+	question: 'How many kids does the couple want to have?',
+	answers: ['0', '1', '2', '3'],
+	answer: 2
+},
+
 ]
